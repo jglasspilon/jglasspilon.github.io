0ef7409f90ea7209ac22489353bcb3b3ac54b015
Julian_Glass-Pilon <julian@astucemedia.com>
2026-01-09 14:04:57 -0500

    Replaced favicons

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,30 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1767982645',resources: [
-      '/Personal-Portfolio-Website/assets/css/jekyll-theme-chirpy.css',
-      '/Personal-Portfolio-Website/',
-      
-        '/Personal-Portfolio-Website/',
-      
-        '/Personal-Portfolio-Website/professional-case-studies/',
-      
-        '/Personal-Portfolio-Website/personal-projects/',
-      
-        '/Personal-Portfolio-Website/contact-me/',
-      
-
-      
-      
-    ],
-
-    interceptor: {paths: [
-        
-      ],urlPrefixes: [
-        
-      ]
-    },
-
-    purge: false
+    purge: true
   
 };
 
